fa04f8a5fc7e6b3eb9a5114c2a126117daa7278c
unknown <hoodiesilver@gmail.com>
2019-07-23 11:10:24 -0400

    first commit

---
+++ js-quiz-questions.js
@@ -0,0 +1,205 @@
+/* [THE "DATABASE" - QUESTIONS, OPTIONS, ANSWERS] */
+// An array that contains objects
+// In the format of {q: QUESTION, o: OPTIONS, a: CORRECT ANSWER}
+var questions = [
+  {
+    q : "Which of these IS NOT a human right?",
+    o : [
+      "Right to own property",
+      "Right to rest and leisure",
+      "Freedom of speech",
+      "Freedom of belief and religion"
+    ],
+    a : 2 // arrays start with 0, so it is 70 meters
+  },
+  {
+    q : "What is the difference between a refugee and an immigrant?",
+    o : [
+      "A refugee is someone who has been in danger and decides to leave his or her country while an immigrant leaves their country for a better life.",
+      "A refugee is someone who has been forced to flee his or her home because of war, violence or persecution, often without warning while an immigrant is someone who makes a conscious decision to leave his or her home and move to a foreign country with the intention of settling there.",
+      "A refugee is only in a certain country temporarily while an immigrant comes to the country permanently.",
+      "There is no difference."
+    ],
+    a : 1
+  },
+  {
+    q : "Around how many people are displaced each year?",
+    o : [
+      "65 million people",
+      "70 million people",
+      "50 million people",
+      "40 million people"
+    ],
+    a : 0
+  },
+  {
+    q : "What percentage of displaced people are children?",
+    o : [
+      "30%",
+      "40%",
+      "50%",
+      "60%"
+    ],
+    a : 2
+  },
+  {
+    q : "Which one of these is considered a vulnerable case during the vetting process?",
+    o : [
+      "Being a single woman of a household",
+      "Being in danger of persecution",
+      "Life being threatened",
+      "Having children with them"
+    ],
+    a : 0
+  },
+  {
+    q : "Which one of these IS NOT a vulnerable case during the vetting process?",
+    o : [
+      "Having medical issues",
+      "Tourture victims",
+      "Single mother of a household",
+      "A child with no guardian"
+    ],
+    a : 3
+  },
+  {
+    q : "What percentage of refugees have a chance at resettling? ",
+    o : [
+      "30%",
+      "1%",
+      "5%",
+      "15%"
+    ],
+    a : 1
+  },
+  {
+    q : "What percentage of refugees are self sufficient within the first year?",
+    o : [
+      "50%",
+      "66%",
+      "75%",
+      "95%"
+    ],
+    a : 3
+  },
+  {
+    q : "What are the three obligatory aspects that states must uphold?",
+    o : [
+      "Protect, Fulfil, Inform",
+      "Govern, Protect, Overlook",
+      "Fulfil, Respect, Protect",
+      "Respect, Govern, Protect"
+    ],
+    a : 2
+  },
+  {
+    q : "Which of these IS NOT adopted international agreement to the UDHR in other countries?",
+    o : [
+      "Rights on civil and political issues",
+      "Rights on economic, social, and cultural issues",
+      "Rights of people with disabilities",
+      "Rights on choice and privacy"
+    ],
+    a : 3
+  },
+  {
+    q : "According to the module, which of the following are ways to promote human rights in your country?",
+    o : [
+      "Write letters and articles to local administrators",
+      "Express yourself artistically",
+      "Organize demonstrations and walk-outs",
+      "All of the above"
+    ],
+    a : 3
+  },
+  {
+    q : "Which of the following is true about the role you should play to implement human rights in your country?",
+    o : [
+      "We have to speak up against injustice, violence or discrimination",
+      "We have to point out if an action or inaction by state authorities is violating human rights",
+      "We all need to play a part in defending and promoting the rights of our national (and international) community",
+      "All of the above"
+    ],
+    a : 3
+  },
+  {
+    q : "When was the UHDR created?",
+    o : [
+      "1948",
+      "1654",
+      "1700",
+      "25 AD"
+    ],
+    a : 0
+  },
+  {
+    q : "Why are human rights necessary?",
+    o : [
+      "To save lives",
+      "To give us freedom",
+      "To protect everyone",
+      "All of the above"
+    ],
+    a : 3
+  },
+  {
+    q : "Which document explains our human rights?",
+    o : [
+      "Declaration of Independence",
+      "UDHR",
+      "The United States Constitution",
+      "All of the above"
+    ],
+    a : 1
+  },
+  {
+    q : "Who created the Universal Declaration of Human Rights?",
+    o : [
+      "Christopher Columbus",
+      "Barack Obama",
+      "The Pope",
+      "Eleanor Roosevelt"
+    ],
+    a : 3
+  },
+  {
+    q : "Who was the first person who decided everyone should have human rights?",
+    o : [
+      "Cyrus the Great",
+      "George Washington",
+      "Abraham Lincoln",
+      "Harriet Tubman"
+    ],
+    a : 0
+  },
+  {
+    q : "How many articles are in the UDHR?",
+    o : [
+      "12",
+      "30",
+      "45",
+      "60"
+    ],
+    a : 1
+  },
+  {
+    q : "Which document is considered to be the first ever decree on human rights?",
+    o : [
+      "The UN Declaration of Human Rights in 1948",
+      "The Napoleonic Code in 1804",
+      "The Cyrus Cylinder in 539 B.C.",
+      "The Magna Carta 1215"
+    ],
+    a : 2
+  },
+  {
+    q : "The Human Rights Act regulates the relationship between individuals and public authorities. Which of the following would not be a public authority? ",
+    o : [
+      "An electric company",
+      "A bank or building society",
+      "A private company running prisons",
+      "A housing association"
+    ],
+    a : 1
+  }
+];
